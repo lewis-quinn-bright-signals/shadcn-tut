404146231b0ea3bd1a7c91a2f6172002ce3fb6e9
Lewis Quinn <lewis.quinn@brightsignals.co.uk>
2024-09-23 13:41:26 +0100

    UPDATE: Lesson 3 init

--- shadcn-ui/src/app/layout.tsx
+++ shadcn-ui/src/app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <nav>
+          <h1>Recipes for ninjas</h1>
+        </nav>
         {children}
       </body>
     </html>
